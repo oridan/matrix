2cd4b0a1299fddaff982c0a4e94a0644f861f0e1
oridan <oridan@gmail.com>
2019-06-06 12:41:16 -0300

    #1: fixed puzzle creation

--- src/app.js
+++ src/app.js
@@ -1,1 +1,1 @@
-//@todo copy and paste de code from https://nodejs.org/en/docs/guides/getting-started-guide/ here.
+// @todo #1 copy and paste de code from https://nodejs.org/en/docs/guides/getting-started-guide/ here.
